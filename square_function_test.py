7eab971cc011529bbaaf52eb4ffb1e0306136c5e
bilalonal <alpfaruk71_@hotmail.com>
2015-02-09 15:40:42 -0600

    Added square_function_test

---
+++ square_function_test.py
@@ -0,0 +1,11 @@
+Python 3.4.2 (v3.4.2:ab2c023a9432, Oct  6 2014, 22:15:05) [MSC v.1600 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> ================================ RESTART ================================
+>>> 
+>>> square(10)
+100
+>>> square(5)
+25
+>>> square(3)
+9
+>>> 
